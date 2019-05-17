56e6aaf2606400c135a338c827d8677e10970be5
Andrzej Kałowski <a.m.kalowski@gmail.com>
2019-05-17 21:15:23 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FirestoreSettingsToken } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { AppRoutingModule } from './app.routing.module';
+import { AppComponent } from './app.component';
+import { environment } from './../environments/environment';
+import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavComponent } from './navigation/sidenav/sidenav.component';
+import { PremisesModule } from './premises/premises.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PageNotFoundComponent } from './navigation/page-not-found/page-not-found.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    CoreModule,
+    PremisesModule,
+    DashboardModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [
+    { provide: FirestoreSettingsToken, useValue: {} }
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './navigation/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {})
+  ],
+  exports: [RouterModule],
+  providers: []
+})
+
+export class AppRoutingModule { }
