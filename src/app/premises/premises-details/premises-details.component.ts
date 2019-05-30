533fde297fc18567f70a7b378debe2a8aa8910e8
Andrzej Kałowski <a.m.kalowski@gmail.com>
2019-05-30 22:39:29 +0200

    Premises details component added

---
+++ src/app/premises/premises-details/premises-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
+import { PremisesService } from './../../services/premises.service';
+import { Premises } from './../../shared/models';
+
+@Component({
+  selector: 'app-premises-details',
+  templateUrl: './premises-details.component.html',
+  styleUrls: ['./premises-details.component.scss']
+})
+
+export class PremisesDetailsComponent implements OnInit {
+  premises$: Observable<Premises>;
+  private id: string;
+
+  constructor(protected activatedRoute: ActivatedRoute,
+              protected premisesService: PremisesService) {}
+
+  ngOnInit(): void {
+    this.getPremises();
+  }
+
+  protected getPremises(): void {
+    this.premises$ = this.activatedRoute.paramMap.pipe(
+      map(params => params.get('id')),
+      switchMap(id => this.premisesService.getPremisesById(id))
+    );
+  }
+
+}
