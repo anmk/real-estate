8ca4d67e6d249d4e5e4b886e6e8994ee2dd830f0
Andrzej Kałowski <a.m.kalowski@gmail.com>
2019-06-03 23:48:10 +0200

    Dashboard module added

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { DashboardComponent } from './dashboard.component';
+import { PremisesFormComponent } from './premises-form/premises-form.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DropZoneDirective } from '../shared/directives/drop-zone.directive';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { FileSizePipe } from '../shared/pipes/file-size.pipe';
+import { PremisesSelectComponent } from './premises-select/premises-select.component';
+import { PremisesInfoComponent } from './premises-info/premises-info.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    PremisesFormComponent,
+    FileUploadComponent,
+    DropZoneDirective,
+    FileSizePipe,
+    PremisesSelectComponent,
+    PremisesInfoComponent
+  ],
+  imports: [
+    DashboardRoutingModule,
+    AngularFireStorageModule,
+    SharedModule
+  ]
+})
+
+export class DashboardModule { }
