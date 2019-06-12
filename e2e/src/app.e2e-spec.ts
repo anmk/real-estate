9f61f06ebfd3a8c0ca30e180c9a9232718f1c410
Andrzej Kałowski <a.m.kalowski@gmail.com>
2019-06-12 20:18:39 +0200

    Upgrade to Angular 8

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to real-estate!');
+    expect(page.getTitleText()).toEqual('Welcome to real-estate2!');
   });
 
   afterEach(async () => {
