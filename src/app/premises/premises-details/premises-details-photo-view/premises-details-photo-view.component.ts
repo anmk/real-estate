017a126092ede390a44b0dc8c673c9c19f7278cb
Andrzej Kałowski <a.m.kalowski@gmail.com>
2019-07-10 19:45:26 +0200

    Premises details photo view component updated

--- src/app/premises/premises-details/premises-details-photo-view/premises-details-photo-view.component.ts
+++ src/app/premises/premises-details/premises-details-photo-view/premises-details-photo-view.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map, switchMap } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
+import { UserInfo } from 'firebase';
 
-import { Photos } from '../../../shared/models';
+import { Photos, Premises } from '../../../shared/models';
 import { PremisesService } from '../../../services/premises.service';
+import { UserService } from '../../../core/user/user.service';
+import { AuthService } from '../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-premises-details-photo-view',
@@ -13,15 +16,24 @@ import { PremisesService } from '../../../services/premises.service';
 })
 
 export class PremisesDetailsPhotoViewComponent implements OnInit, OnDestroy {
+
+  premises$: Observable<Premises>;
   image: Photos;
   private id: string;
+  private pid: string;
+  user: UserInfo = this.userService.currentUser;
+  auth: boolean = this.authService.isAuth();
   private dataSubscription: Subscription;
+  private deleteSubscription: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private premisesService: PremisesService) { }
+              private premisesService: PremisesService,
+              private userService: UserService,
+              private authService: AuthService) {}
 
   ngOnInit(): void {
     this.getImage();
+    this.getPremises();
   }
 
   private getImage(): void {
@@ -31,12 +43,33 @@ export class PremisesDetailsPhotoViewComponent implements OnInit, OnDestroy {
       map(params => params.get('pid')),
       switchMap(pid => this.premisesService.getPhotoByLink(`premises/${this.id}/imageUrls/${pid}`))
     )
-    .subscribe(image => this.image = image);
+    .subscribe(image => {
+      this.image = image;
+    });
+  }
+
+  private getPremises() {
+    this.premises$ = this.premisesService.getPremisesById(this.id);
+  }
+
+  delete() {
+    this.id = this.activatedRoute.snapshot.parent.url[1].path;
+
+    this.deleteSubscription = this.activatedRoute.paramMap.pipe(
+      map(params => params.get('pid'))
+    )
+    .subscribe(pid =>  {
+      this.pid = pid;
+    });
+    this.premisesService.deleteImage(this.id, this.pid, this.image.nameInStorage);
   }
 
   ngOnDestroy(): void {
     if (this.dataSubscription) {
       this.dataSubscription.unsubscribe();
+    }
+    if (this.deleteSubscription) {
+      this.deleteSubscription.unsubscribe();
     }
   }
 
