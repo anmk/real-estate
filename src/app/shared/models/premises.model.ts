26c105894b0aafbb94332a53e1fd168143a64978
Andrzej Kałowski <a.m.kalowski@gmail.com>
2019-08-01 20:42:33 +0200

    Premises model updated

--- src/app/shared/models/premises.model.ts
+++ src/app/shared/models/premises.model.ts
@@ -4,16 +4,16 @@ export interface Premises {
     id: string;
     userId: string;
     country: string;
+    premises: 'room' | 'studio' | '';
     city: string;
     street: string;
     streetNumber: string;
-    flat: string | null;
+    flat: string | '';
     area: number | null;
-    premises: 'room' | 'studio';
-    heating: 'gas' | 'electric' | 'district' | null;
-    rented: boolean;
+    additionalInformations: string | '';
+    heating: 'gas' | 'electric' | 'district' | '';
     fornished: boolean;
+    rented: boolean;
     thumbnail: Photos;
-    additionalInformations: string | null;
     time: number;
   }
